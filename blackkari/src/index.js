b8939ff09cdc04a5f4ceea9a4f8bc34030f285b2
Toni Suominen <toni.suominen@vincit.fi>
2019-11-02 11:56:44 +0200

    Add scheduling

--- blackkari/src/index.js
+++ blackkari/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const schedule = require('node-schedule')
 const app = express()
 const port = 3000
 
@@ -9,4 +10,9 @@ app.get('/hasNotifications', (req, res) => res.send({notifications: false}))
 app.get('/getImage', (req, res) => res.send('Hello World!'))
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+    const scheduling = schedule.scheduleJob('* * * * *', function(){
+        console.log('The answer to life, the universe, and everything!')
+    })
+})
