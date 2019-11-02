6c624e46e4ed8aa19ecace58ccf51c3e26bbf68c
Toni Suominen <toni.suominen@vincit.fi>
2019-11-02 12:24:41 +0200

    Add fact fetch

--- blackkari/src/facts/factService.js
+++ blackkari/src/facts/factService.js
@@ -1,10 +1,18 @@
+const fetch = require('node-fetch');
 
 module.exports = function() {
     this.dailyFact = getDailyFact
 }
 
 function getDailyFact() {
+    fetch('https://uselessfacts.jsph.pl/random.json?language=en')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.text)
+        })
+        .catch(err =>  {console.log(err)})
     console.log("FACT OF DAYYDAYYAYAYA")
+
 }
 
 
