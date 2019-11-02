31ca5599e5d64a8f38726693d65b3402e06d5b9c
kyosti <kyosti.puro@vincit.fi>
2019-11-02 12:22:04 +0200

    Adds endpoints for fetching info

--- blackkari/src/index.js
+++ blackkari/src/index.js
@@ -1,15 +1,33 @@
 const express = require('express')
 const schedule = require('node-schedule')
 const facts = require('./facts/factService')
+const path = require('path')
+const fs = require('fs')
 const app = express()
 const port = 3000
 
+const THREE = 3
+
+const isThreeOClock = () =>
+  new Date().getHours() === THREE
+
 app.get('/ping', (req, res) => res.send('Hello World!'))
 
-app.get('/hasNotifications', (req, res) => res.send({notifications: false}))
+app.get('/hasNotifications', (req, res) => res.send({notifications: isThreeOClock()}))
 
-app.get('/getImage', (req, res) => res.send('Hello World!'))
+app.get('/getImage', (req, res) => {
+  const file = path.join(__dirname, "../resources/test-image.jpg")
+  const s = fs.createReadStream(file)
 
+  if (!isThreeOClock()) {
+    return res.status(403).end("It is not three o'clock!")
+  }
+
+  s.on('open', function () {
+    res.set('Content-Type', 'image/jpeg')
+    s.pipe(res)
+  })
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
