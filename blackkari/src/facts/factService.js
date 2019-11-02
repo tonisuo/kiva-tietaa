f635fe310a9321d7ffd2acc7546099f4a861bbd9
Toni Suominen <toni.suominen@vincit.fi>
2019-11-02 15:05:29 +0200

    Add docker compose for mongodb and save function for facts

--- blackkari/src/facts/factService.js
+++ blackkari/src/facts/factService.js
@@ -1,18 +1,20 @@
 const fetch = require('node-fetch');
+const db = require('../db/db.js')
 
 module.exports = function() {
     this.dailyFact = getDailyFact
 }
 
 function getDailyFact() {
+    const dbInstance = new db()
+
     fetch('https://uselessfacts.jsph.pl/random.json?language=en')
         .then(response => response.json())
         .then(data => {
-            console.log(data.text)
+            dbInstance.save(data.text)
         })
         .catch(err =>  {console.log(err)})
     console.log("FACT OF DAYYDAYYAYAYA")
-
 }
 
 
