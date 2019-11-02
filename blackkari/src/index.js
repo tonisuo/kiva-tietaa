face1833478d9d392b195e16599a189872974733
kyosti <kyosti.puro@vincit.fi>
2019-11-02 11:47:25 +0200

    Adds dev for hot reload server

--- blackkari/src/index.js
+++ blackkari/src/index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/ping', (req, res) => res.send('Hello World!'))
+
+app.get('/hasNotifications', (req, res) => res.send({notifications: false}))
+
+app.get('/getImage', (req, res) => res.send('Hello World!'))
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
